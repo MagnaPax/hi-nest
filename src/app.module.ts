f26854e3465eb0a3230a752ee27d2daff532ec88
MagnaPax <chunhee.han@outlook.com>
2023-05-08 00:43:46 +0900

    feat: 삭제&생성
    기본 컨트롤러, 서비스 삭제 후 movies 컨트롤러 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 // @ <- 데코레이터: 클래스에 함수 기능을 추가. 클래스 '위'의 함수. 클래스 '위'에 데코레이션 장식
+// url을 가져오고 함수를 실행. express의 컨트롤러/라우터
 @Module({
   imports: [],
-  controllers: [AppController], // express의 컨트롤러/라우터: url을 가져오고 함수를 실행
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [],
 })
 export class AppModule {}
